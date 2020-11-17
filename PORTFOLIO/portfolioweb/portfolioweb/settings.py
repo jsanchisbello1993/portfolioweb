8630d253cefeedfc3432e93b113350674c744de4
jsanchisbello1993 <69072919+jsanchisbello1993@users.noreply.github.com>
2020-11-17 12:18:56 +0100

    Update settings.py

--- PORTFOLIO/portfolioweb/portfolioweb/settings.py
+++ PORTFOLIO/portfolioweb/portfolioweb/settings.py
@@ -129,12 +129,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'josem.sb1993@gmail.com'
-EMAIL_HOST_PASSWORD = 'uzktljepbwiuxiqz' #past the key or password app here
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'default from email'
+
+
 
